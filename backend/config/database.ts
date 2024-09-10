89f53b41f616f6cb8d21285ef00c57a918a0167e
ShaherAshraf <shaherashraf77@gmail.com>
2024-09-10 21:26:57 +0300

    Modify config/databas.ts env variables

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -29,13 +29,35 @@ export default ({ env }) => {
         max: env.int("DATABASE_POOL_MAX", 10),
       },
     },
+    // FOR LOCAL USE
+    // mysql2: {
+    //   connection: {
+    //     host: env("DATABASE_HOST", "localhost"),
+    //     port: env.int("DATABASE_PORT", 3306),
+    //     database: env("DATABASE_NAME", "gdm_branding"),
+    //     user: env("DATABASE_USERNAME", "root"),
+    //     password: env("DATABASE_PASSWORD", "sh31121995"),
+    //     ssl: env.bool("DATABASE_SSL", false) && {
+    //       key: env("DATABASE_SSL_KEY", undefined),
+    //       cert: env("DATABASE_SSL_CERT", undefined),
+    //       ca: env("DATABASE_SSL_CA", undefined),
+    //       capath: env("DATABASE_SSL_CAPATH", undefined),
+    //       cipher: env("DATABASE_SSL_CIPHER", undefined),
+    //       rejectUnauthorized: env.bool(
+    //         "DATABASE_SSL_REJECT_UNAUTHORIZED",
+    //         true
+    //       ),
+    //     },
+    //   },
+
+    //FOR DEPLOYMENT ONLY
     mysql2: {
       connection: {
-        host: env("DATABASE_HOST", "localhost"),
+        host: env("DATABASE_HOST"),
         port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "gdm_branding"),
-        user: env("DATABASE_USERNAME", "root"),
-        password: env("DATABASE_PASSWORD", "sh31121995"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
